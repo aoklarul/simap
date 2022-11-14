6e8bf0fb9fc4db124fdf6c0555f75a32e5d1b4d8
averno99 <mfebri.nugroho99@gmail.com>
2022-11-14 14:18:23 +0700

    CRUD kelurahan

--- application/controllers/KelurahanController.php
+++ application/controllers/KelurahanController.php
@@ -6,13 +6,125 @@ class KelurahanController extends CI_Controller
   public function __construct()
   {
     parent::__construct();
+    $this->load->library('form_validation');
+    $this->load->model('Kelurahan');
+    $this->load->model('Kecamatan');
   }
 
   public function index()
   {
     $this->template->load('main/app', 'kelurahan/index', [
       'title' => 'Kelurahan',
-      'page_title' => 'Data Kelurahan'
+      'page_title' => 'Data Kelurahan',
+      'kelurahan' => $this->Kelurahan->get()->result()
     ]);
+  }
+
+  public function create()
+  {
+    $this->form_validation->set_rules('nama', 'Kelurahan', 'required|trim');
+    if (empty($_FILES['geojson']['name'])) {
+      $this->form_validation->set_rules('geojson', 'Geojson', 'required|trim');
+    }
+
+    if ($this->form_validation->run() == FALSE) {
+      $this->template->load('main/app', 'kelurahan/create', [
+        'title' => 'Kelurahan',
+        'page_title' => 'Tambah Data Kelurahan',
+        'kecamatan' => $this->Kecamatan->get()->result()
+      ]);
+    } else {
+      if (!empty($_FILES['geojson']['name'])) {
+        $config['upload_path']          = './assets/geojson/kelurahan/';
+        $config['allowed_types']        = 'geojson';
+
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload('geojson')) {
+          echo $this->upload->display_errors();
+          die;
+        } else {
+          $file_name = $this->upload->data('file_name');
+          $nama = $this->input->post('nama');
+          $idKecamatan = $this->input->post('idKecamatan');
+
+          $data = [
+            'idKecamatan' => $idKecamatan,
+            'nama' => $nama,
+            'geojson' => $file_name
+          ];
+        }
+      }
+
+      $this->Kelurahan->add($data);
+      $this->session->set_flashdata('message', 'Data berhasil ditambahkan!');
+      redirect('kelurahan');
+    }
+  }
+
+  public function edit($id)
+  {
+    $this->form_validation->set_rules('nama', 'Kelurahan', 'required|trim');
+    $kelurahan = $this->Kelurahan->find($id)->row_array();
+
+    if ($this->form_validation->run() == FALSE) {
+      $this->template->load('main/app', 'kelurahan/edit', [
+        'title' => 'Kelurahan',
+        'page_title' => 'Ubah Data Kelurahan',
+        'kelurahan' => $this->Kelurahan->find($id)->row(),
+        'kecamatan' => $this->Kecamatan->get()->result()
+      ]);
+    } else {
+      if (!empty($_FILES['geojson']['name'])) {
+        $config['upload_path']          = './assets/geojson/kelurahan/';
+        $config['allowed_types']        = 'geojson';
+
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload('geojson')) {
+          echo $this->upload->display_errors();
+          die;
+        } else {
+          $old_geojson = $kelurahan['geojson'];
+          unlink(FCPATH . 'assets/geojson/kelurahan/' . $old_geojson);
+
+          $file_name = $this->upload->data('file_name');
+          $nama = $this->input->post('nama');
+          $idKecamatan = $this->input->post('idKecamatan');
+
+          $data = [
+            'idKecamatan' => $idKecamatan,
+            'nama' => $nama,
+            'geojson' => $file_name
+          ];
+        }
+      }
+
+      if (empty($_FILES['geojson']['name'])) {
+        $nama = $this->input->post('nama');
+        $idKecamatan = $this->input->post('idKecamatan');
+
+        $data = [
+          'idKecamatan' => $idKecamatan,
+          'nama' => $nama
+        ];
+      }
+
+      $this->Kelurahan->edit($data, $id);
+      $this->session->set_flashdata('message', 'Data berhasil diubah!');
+      redirect('kelurahan');
+    }
+  }
+
+  public function delete($id)
+  {
+    $kelurahan = $this->Kelurahan->find($id)->row_array();
+
+    $old_geojson = $kelurahan['geojson'];
+    unlink(FCPATH . 'assets/geojson/kelurahan/' . $old_geojson);
+
+    $this->Kelurahan->delete($id);
+    $this->session->set_flashdata('message', 'Data berhasil dihapus!');
+    redirect('kelurahan');
   }
 }
--- application/views/kelurahan/index.php
+++ application/views/kelurahan/index.php
@@ -12,14 +12,106 @@
 <!-- CONTENT AREA -->
 <div class="row layout-top-spacing">
 
-  <div class="col-12">
-    <div class="alert alert-arrow-right alert-icon-right alert-light-warning alert-dismissible fade show mb-4" role="alert">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-circle">
-        <circle cx="12" cy="12" r="10"></circle>
-        <line x1="12" y1="8" x2="12" y2="12"></line>
-        <line x1="12" y1="16" x2="12" y2="16"></line>
+  <div class="action-btn mb-3">
+    <a href="<?= site_url('kelurahan/create') ?>" type="button" class="btn btn-secondary">
+      <span class="btn-text-inner">Tambah Data</span>
+    </a>
+  </div>
+
+  <?php if ($this->session->flashdata('message')) : ?>
+    <div class="alert alert-icon-left alert-light-success alert-dismissible fade show mb-4" role="alert">
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x close" data-bs-dismiss="alert">
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line>
+        </svg>
+      </button>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check">
+        <polyline points="20 6 9 17 4 12"></polyline>
       </svg>
-      <strong>HALAMAN KELURAHAN</strong> Learn more about Starter Kit by refering to the <a target="_blank" href="https://designreset.com/cork/documentation/getting-started.html">Documentation</a>
+      <?= $this->session->flashdata('message') ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="col-lg-12 col-12 layout-spacing">
+    <div class="statbox widget box box-shadow">
+      <div class="widget-content widget-content-area">
+
+        <table class="table dt-table-hover" id="zero-config" style="width:100%">
+          <thead>
+            <tr>
+              <th class="text-center" scope="col">No</th>
+              <th scope="col">Kelurahan</th>
+              <th scope="col">Kecamatan</th>
+              <th scope="col">Geojson</th>
+              <th class="text-center dt-no-sorting" scope="col">Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $no = 1;
+            foreach ($kelurahan as $row) :
+            ?>
+              <tr>
+                <td class="text-center"><?= $no ?></td>
+                <td><?= $row->kelurahan ?></td>
+                <td><?= $row->kecamatan ?></td>
+                <td>
+                  <a href="<?= base_url('assets/geojson/kelurahan/') . $row->geojson ?>" target="_blank">
+                    <?= $row->geojson ?>
+                  </a>
+                </td>
+                <td class="text-center">
+                  <div class="dropdown">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal">
+                        <circle cx="12" cy="12" r="1"></circle>
+                        <circle cx="19" cy="12" r="1"></circle>
+                        <circle cx="5" cy="12" r="1"></circle>
+                      </svg>
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink1">
+                      <a class="dropdown-item" href="<?= site_url('kelurahan/edit/') . $row->id ?>">Ubah Data</a>
+                      <a class="dropdown-item" href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#btn-delete<?= $row->id ?>">Hapus Data</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            <?php
+              $no++;
+            endforeach;
+            ?>
+          </tbody>
+        </table>
+
+        <!-- Modal -->
+        <?php foreach ($kelurahan as $row) : ?>
+          <div class="modal fade" id="btn-delete<?= $row->id ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalCenterTitle">Konfirmasi Hapus Data</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+                      <line x1="18" y1="6" x2="6" y2="18"></line>
+                      <line x1="6" y1="6" x2="18" y2="18"></line>
+                    </svg>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p class="modal-text">Apakah anda yakin ingin menghapus data?</p>
+                </div>
+                <div class="modal-footer">
+                  <a type="button" class="btn btn-light-dark" data-bs-dismiss="modal">Tidak</a>
+                  <a type="button" class="btn btn-primary" href="<?= site_url('kelurahan/delete/') . $row->id ?>">Hapus Data</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php endforeach; ?>
+
+      </div>
     </div>
   </div>
 
